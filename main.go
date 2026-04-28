b8da873effe9f3523f3f3db03ddecde7d82346cd
faymndev <faymndev@gmail.com>
2026-04-28 14:20:33 -0700

    add command history with the liner package

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
+	"github.com/peterh/liner"
 )
 
 func main() {
 	commands := newCommandRegistry()
 	context := newCliContext()
 
-	scanner := bufio.NewScanner(os.Stdin)
+	line := liner.NewLiner()
+	defer line.Close()
+
+	line.SetCtrlCAborts(true)
+
 	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		context.input = cleanInput(scanner.Text())
-		command, ok := commands.get(context.input[0])
-		if !ok {
-			fmt.Println("invalid command")
+		input, err := line.Prompt("Pokedex> ")
+		if err == liner.ErrPromptAborted {
+			command, _ := commands.get("exit")
+			command.callback(&context)
+			return
+		} else if err != nil {
+			// if something is wrong with our prompting mechanism we should just exit
+			panic(err)
+		}
+
+		line.AppendHistory(input)
+		context.input = cleanInput(input)
+		if len(input) == 0 {
+			fmt.Printf("You didn't type a command!")
 			continue
 		}
 
-		err := command.callback(&context)
+		command, ok := commands.get(context.input[0])
+		if !ok {
+			fmt.Printf("Invalid command %s\n", context.input[0])
+			continue
+		}
+
+		err = command.callback(&context)
 		if err != nil {
 			fmt.Println(err)
 		}
